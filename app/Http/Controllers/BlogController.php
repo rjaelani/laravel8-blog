5fe04822ad0ae8fb41d926c4853ad70fd2f90244
Rizal zaelani 72 <rjaelani50@gmail.com>
2021-03-04 10:24:46 +0700

    initial commit

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Blog;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class BlogController extends Controller
+{
+    public function index()
+    {
+        $blogs = Blog::latest()->paginate(10);
+        return view ('blog.index', compact('blogs'));
+    }
+
+    public function create()
+    {
+        return view('blog.create');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'image'   => 'required|image|mimes:png,jpg,jpeg',
+            'title'   => 'required',
+            'content' => 'required' 
+        ]);
+
+        //upload image
+        $image = $request->file('image');
+        $image->storeAs('public/blogs', $image->hashName());
+
+        $blog = Blog::create([
+            'image'   => $image->hashName(),
+            'title'   => $request->title,
+            'content' => $request->content
+        ]);
+
+        if($blog){
+            //redirect dengan pesan sukses
+            return redirect()->route('blog.index')->with(['success' => 'data berhasil disimpan!']);
+        }else{
+            //redirect dengan pesan error
+            return redirect()->route('blog.index')->with(['error' => 'data gagal disimpan!']);
+        }
+    }
+
+    public function edit(Blog $blog)
+    {
+        return view('blog.edit', compact('blog'));
+    }
+
+    public function update(Request $request, Blog $blog)
+    {
+        $this->validate($request, [
+            'title'   => 'required',
+            'content' => 'required'
+        ]);
+
+        //get data Blog by ID
+        $blog = Blog::findOrFail($blog->id);
+
+        if($request->file('image') == "") {
+            $blog->update([
+                'title'  => $request->title,
+                'content' => $request->content
+            ]);
+        } else {
+
+            //hapus old image
+            Storage::disk('local')->delete('public/blogs/'.$blog->image);
+
+            //upload new image
+            $image = $request->file('image');
+            $image->storeAs('public/blogs', $image->hashName());
+
+            $blog->update([
+                'image'   => $image->hashName(),
+                'title'   => $request->title,
+                'content' => $request->content
+            ]);
+        }
+
+        if($blog){
+            //redirect dengan pesan sukses
+            return redirect()->route('blog.index')->with(['success' => 'data berhasil diupdate!']);
+        }else{
+            //redirect dengan pesan error
+            return redirect()->route('blog.index')->with(['error' => ' data gagal diupdate!']);
+        }
+    }
+
+    public function destroy($id)
+    {
+        $blog = Blog::findOrFail($id);
+        Storage::disk('local')->delete('public/blogs/'.$blog->image);
+        $blog->delete();
+
+        if($blog){
+            //redirect dengan pesan sukses
+            return redirect()->route('blog.index')->with(['success' => 'data berhasil dihapus!']);
+        }else{
+            //redirect dengan pesan gagal
+            return redirect()->route('blog.index')->with(['error' => 'data gagal dihapus!']);
+        }
+    }
+}
